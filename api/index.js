ab6e650618316580f965cc4865a62ae6a5794e47
unknown <tonyekim>
2024-01-20 16:08:47 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb+srv://tony:otoly1992@mern-blog.whnjoda.mongodb.net/mern-blog?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
