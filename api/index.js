3c4f3888e1fd895254e9f6bc25dd2ffbd4ad5dc9
unknown <tonyekim>
2024-01-20 20:33:44 +0100

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRoutes from './routes/user.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose
-  .connect("mongodb+srv://tony:otoly1992@mern-blog.whnjoda.mongodb.net/mern-blog?retryWrites=true&w=majority")
+  .connect(
+    "mongodb+srv://tony:otoly1992@mern-blog.whnjoda.mongodb.net/mern-blog?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("MongoDB is connected");
   })
@@ -11,10 +14,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
-
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
