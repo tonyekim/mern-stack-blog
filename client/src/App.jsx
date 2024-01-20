017d321c3da78d0f74509508d9bb37f6bf8092c4
unknown <tonyekim>
2024-01-20 15:41:37 +0100

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,19 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-
+import Project from "./pages/Project";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />  
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sig-in" element={<SignIn />} />
+        <Route path="/project" element={<Project />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
